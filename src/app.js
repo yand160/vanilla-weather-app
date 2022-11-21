18256aea86833197c666cf5abaadc5a7e4bf2833
yand160 <74022340+yand160@users.noreply.github.com>
2022-11-21 17:31:37 -0500

    Revised app.js and style.css

--- src/app.js
+++ src/app.js
@@ -22,17 +22,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  days.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
                             <div class="col-2">
                                 <div class="weather-forecast-date">${day}(</div>
                                 <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"@2x.png" alt="" width="42" />
@@ -42,6 +57,7 @@ function displayForecast(response) {
                                 </div>
                             </div>
                         `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -97,5 +113,3 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("New York");
-
-
